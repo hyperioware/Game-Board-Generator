73a543cd8c4dac52c720ba95e4b04893156fa0bf
Michael Rudd <audiostratus@yahoo.com>
2020-06-10 15:43:38 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-
+class Board{
+  height = 0;
+  width = 0;
+  name = "";
+  tiles = {};
+  constructor(settings){
+    this.height = settings.height;
+    this.width = settings.width;
+    this.name = settings.name;
+    this.tiles = settings.tiles;
+  }
+}
